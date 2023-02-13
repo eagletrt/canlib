f24b4ecc0e7565126f2ea316a8f7fbf3c09675ac
Filippo <pippo.faccini@gmail.com>
2023-02-13 16:16:50 +0100

    fix: lint

--- canlib/generators/proto/mapping/__init__.py
+++ canlib/generators/proto/mapping/__init__.py
@@ -8,7 +8,9 @@ from canlib.common.schema import Schema
 BASE_DIR = Path(__file__).parent
 
 TEMPLATE_MAPPING_CPP = j2.Template((BASE_DIR / "mapping.h.j2").read_text())
-TEMPLATE_PROTO_INTERFACE_CPP = j2.Template((BASE_DIR / "proto_interface.h.j2").read_text())
+TEMPLATE_PROTO_INTERFACE_CPP = j2.Template(
+    (BASE_DIR / "proto_interface.h.j2").read_text()
+)
 
 
 def generate(network: Network, schema: Schema, output_dir: Path):
@@ -17,4 +19,6 @@ def generate(network: Network, schema: Schema, output_dir: Path):
     mapping_path.write_text(TEMPLATE_MAPPING_CPP.render(network=network, schema=schema))
 
     mapping_path = (output_dir / "cpp") / "proto_interface.h"
-    mapping_path.write_text(TEMPLATE_PROTO_INTERFACE_CPP.render(network=network, schema=schema))
+    mapping_path.write_text(
+        TEMPLATE_PROTO_INTERFACE_CPP.render(network=network, schema=schema)
+    )
