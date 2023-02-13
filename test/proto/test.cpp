15b0a5b357e947041024540eade6263c91d6b670
Filippo <pippo.faccini@gmail.com>
2023-02-13 13:17:12 +0100

    Fixed signals vs enums deserialization

--- test/proto/test.cpp
+++ test/proto/test.cpp
@@ -1,16 +1,11 @@
 #include <stdio.h>
 
-#define test_NETWORK_IMPLEMENTATION
-#include "lib/test/c/network.h"
-
-#define test_IDS_IMPLEMENTATION
-#include "lib/test/c/ids.h"
-
-#define test_WATCHDOG_IMPLEMENTATION
-#include "lib/test/c/watchdog.h"
-
-#define test_MAPPING_IMPLEMENTATION
-#include "proto/test/cpp/mapping.h"
+#define CANLIB_TIMESTAMP
+#define bms_NETWORK_IMPLEMENTATION
+#define bms_PROTO_INTERAFCE_IMPLEMENTATION
+#include "lib/bms/c/ids.h"
+#include "proto/bms/cpp/bms.pb.h"
+#include "proto/bms/cpp/proto_interface.h"
 
 void print_byte_as_bits(char val) {
   for (int i = 7; 0 <= i; i--) {
@@ -28,22 +23,42 @@ void print_bits(unsigned char* bytes, size_t num_bytes) {
 }
 
 int main() {
-  // ENUMS
-  uint8_t data[8] = {0};
-  test_serialize_CAR_STATUS(data, test_InverterStatus_ON,
-                            test_InverterStatus_ON, test_CarStatus_RUN);
+  bms::Pack pack;
+  bms_devices* devs = bms_devices_new();
 
-  printf("CAR_STATUS ");
-  print_bits(data, 8);
+  ((bms_message_BOARD_STATUS*)devs[bms_INDEX_BOARD_STATUS_CELLBOARD0]->message_raw)->_timestamp = 1;
+  ((bms_message_BOARD_STATUS*)devs[bms_INDEX_BOARD_STATUS_CELLBOARD0]->message_raw)->errors = 1000;
+  for(int i = 0; i < 100; i++){
+    bms_proto_serialize_from_id(bms_ID_BOARD_STATUS_CELLBOARD0, &pack, devs);
+  }
 
-  // BITSETS
-  memset(data, 0, 8);
-  test_serialize_DAS_ERRORS(data, test_DasErrors_FSM |
-                                      test_DasErrors_INVL_TOUT |
-                                      test_DasErrors_PEDAL_IMPLAUSIBILITY);
+  network_enums net_enums;
+  network_signals net_signals;
+  network_strings net_strings;
 
-  printf("DAS_ERRORS ");
-  print_bits(data, 8);
+  bms_proto_deserialize(&pack, &net_enums, &net_signals, &net_strings, 0);
+
+  for(const auto& message : net_signals){
+    for(const auto& signal : message.second){
+      for(int i = 0; i < signal.second.size(); i++){
+        printf("signal\t\t%s.%s: %f\n", message.first.c_str(), signal.first.c_str(), signal.second[i]);
+      }
+    }
+  }
+  for(const auto& message : net_enums){
+    for(const auto& signal : message.second){
+      for(int i = 0; i < signal.second.size(); i++){
+        printf("enum\t\t%s.%s: %" PRIu64 "\n", message.first.c_str(), signal.first.c_str(), signal.second[i]);
+      }
+    }
+  }
+  for(const auto& message : net_strings){
+    for(const auto& signal : message.second){
+      for(int i = 0; i < signal.second.size(); i++){
+        printf("string\t\t%s.%s: %s\n", message.first.c_str(), signal.first.c_str(), signal.second[i].c_str());
+      }
+    }
+  }
 
   return 0;
 }
